ff76335bb67113e2b9d63c302864e4a012faae15
dsypasit <ong.pasit@gmail.com>
2023-01-06 00:47:00 +0700

    feat: add twosum function

--- golang/twosum/twosum.go
+++ golang/twosum/twosum.go
@@ -1,1 +1,16 @@
 package twosum
+
+import "errors"
+
+func twosum(list []int, expected int) ([2]int, error) {
+	temp := make(map[int]int)
+	for i, v := range list {
+		if _, ok := temp[expected-v]; !ok {
+			temp[expected-v] = i
+		}
+		if r, ok := temp[v]; ok {
+			return [2]int{list[r], v}, nil
+		}
+	}
+	return [2]int{}, errors.New("no value")
+}
