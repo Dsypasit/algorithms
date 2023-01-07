9b1d56e8f16a16155435b8757916ff1b415eb03e
dsypasit <ong.pasit@gmail.com>
2023-01-07 09:01:17 +0700

    refactoring: use pointer array instead of array

--- golang/twosum/twosum.go
+++ golang/twosum/twosum.go
@@ -1,16 +1,14 @@
 package twosum
 
-import "errors"
-
-func twosum(list []int, expected int) ([2]int, error) {
+func twosum(list []int, expected int) *[]int {
 	temp := make(map[int]int)
 	for i, v := range list {
 		if _, ok := temp[expected-v]; !ok {
 			temp[expected-v] = i
 		}
 		if r, ok := temp[v]; ok {
-			return [2]int{list[r], v}, nil
+			return &[]int{list[r], v}
 		}
 	}
-	return [2]int{}, errors.New("no value")
+	return nil
 }
--- golang/twosum/twosum_test.go
+++ golang/twosum/twosum_test.go
@@ -1,7 +1,6 @@
 package twosum
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,26 +11,25 @@ func TestTwoSum(t *testing.T) {
 		name          string
 		inputList     []int
 		inputExpected int
-		want          [2]int
+		want          *[]int
 	}{
 		{
 			name:          "input [1,2,3] expected 5 should be [2,3]",
 			inputList:     []int{1, 2, 3},
 			inputExpected: 5,
-			want:          [2]int{2, 3},
+			want:          &[]int{2, 3},
 		},
 		{
 			name:          "input [7,9,2,3,1] expected 10 should be [7,3]",
 			inputList:     []int{7, 9, 2, 3, 1},
 			inputExpected: 10,
-			want:          [2]int{7, 3},
+			want:          &[]int{7, 3},
 		},
 	}
 
 	for _, tt := range input {
 		t.Run(tt.name, func(t *testing.T) {
-			output, err := twosum(tt.inputList, tt.inputExpected)
-			assert.Nil(t, err)
+			output := twosum(tt.inputList, tt.inputExpected)
 			assert.Equal(t, tt.want, output)
 		})
 	}
@@ -42,20 +40,20 @@ func TestTwoSumError(t *testing.T) {
 		name          string
 		inputList     []int
 		inputExpected int
-		want          error
+		want          *[]int
 	}{
 		{
 			name:          "input [] expected 10 should be [0,0]",
 			inputList:     []int{},
 			inputExpected: 10,
-			want:          errors.New("no value"),
+			want:          nil,
 		},
 	}
 
 	for _, tt := range input {
 		t.Run(tt.name, func(t *testing.T) {
-			_, err := twosum(tt.inputList, tt.inputExpected)
-			assert.Equal(t, tt.want, err)
+			output := twosum(tt.inputList, tt.inputExpected)
+			assert.Nil(t, tt.want, output)
 		})
 	}
 }
